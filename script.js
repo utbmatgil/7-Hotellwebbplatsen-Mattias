d42e498c6a6d35bc95f0bdc19e359511a37a26d3
utbmatgil <112627936+utbmatgil@users.noreply.github.com>
2023-06-02 17:57:36 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function clearForm() {
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("country").value = "australia";
+    document.getElementById("subject").value = "";
+
+    document.getElementById("myForm").reset();
+}
